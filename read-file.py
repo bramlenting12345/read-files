e5738b475d5375e7ac2402646e0e0a8dcefe238e
bramlenting1234 <99068035@mydavinci.nl>
2022-03-14 09:54:17 +0100

    read-file-klaar

--- read-file.py
+++ read-file.py
@@ -1,1 +1,12 @@
-print ("test")
+from time import time
+import time
+
+status = True 
+bestand  = open('README.md','r')
+
+
+while status == True:
+    time.sleep(1)
+    lezen = bestand.readline()
+    print(lezen)
+
